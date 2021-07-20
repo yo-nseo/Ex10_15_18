950e7fc6bea2af6e8a3c1f428a0ab88bfe19f29a
YoonSeo4618 <w2035@e-mirim.hs.kr>
2021-07-20 13:38:06 +0900

    completion

--- app/src/main/java/kr/hs/emirim/ohyoonseo/ex10_15_18/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ohyoonseo/ex10_15_18/MainActivity.java
@@ -8,11 +8,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText edit1 = findViewById(R.id.edit1);
-    EditText edit2 = findViewById(R.id.edit2);
+    EditText edit1;
+    EditText edit2;
+    RadioGroup rg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         edit1 = findViewById(R.id.edit1);
         edit2 = findViewById(R.id.edit2);
+        rg = findViewById(R.id.rg);
+
         Button btnPlus = findViewById(R.id.btn_plus);
 
         btnPlus.setOnClickListener(btnListener);
@@ -31,9 +35,26 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            char op = 0;
+            switch (rg.getCheckedRadioButtonId()) {
+                case R.id.radio_plus:
+                    op = '+';
+                    break;
+                case R.id.radio_minus:
+                    op = '-';
+                    break;
+                case R.id.radio_multi:
+                    op = '*';
+                    break;
+                case R.id.radio_divide:
+                    op = '/';
+                    break;
+            }
+
             Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
             intent.putExtra("num1", Integer.parseInt(edit1.getText().toString()));
             intent.putExtra("num2", Integer.parseInt(edit2.getText().toString()));
+            intent.putExtra("op", op);
             startActivityForResult(intent, 0);
         }
     };
@@ -43,8 +64,8 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (resultCode == RESULT_OK) {
-            int sum = data.getIntExtra("sum", 0);
-            Toast.makeText(getApplicationContext(), "합계 : " + sum, Toast.LENGTH_SHORT).show();
+            int result = data.getIntExtra("result", 0);
+            Toast.makeText(getApplicationContext(), "합계 : " + result, Toast.LENGTH_SHORT).show();
         }
     }
 }
--- app/src/main/java/kr/hs/emirim/ohyoonseo/ex10_15_18/SecondActivity.java
+++ app/src/main/java/kr/hs/emirim/ohyoonseo/ex10_15_18/SecondActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
-    int sum;
+    int result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +18,22 @@ public class SecondActivity extends AppCompatActivity {
         Intent intent = getIntent();
         int num1 = intent.getIntExtra("num1", 0);
         int num2 = intent.getIntExtra("num2", 0);
-        sum = num1 + num2;
+
+        char op = intent.getCharExtra("op", '0');
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+        }
 
         Button btnBack = findViewById(R.id.btn_back);
         btnBack.setOnClickListener(btnListener);
@@ -28,7 +43,7 @@ public class SecondActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
-            mainIntent.putExtra("sum", sum);
+            mainIntent.putExtra("result", result);
             setResult(RESULT_OK, mainIntent);
             finish();
         }
